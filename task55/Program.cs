419a4f934c562dfbcfcecc3ca20f2843c9d0b4d0
naumovanatalia <naumova.na43@mail.ru>
2022-12-24 11:25:42 +0200

    add task55

---
+++ task55/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы.
+
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 10)
+{
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matr;
+}
+
+// метод для печати (вывода в консоль) двумерного массива
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+        
+    }
+}
+
+int[,] matrix = GetRandomMatrix(3, 5);
+PrintMatrix(matrix);
+
+int[,] newMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+for (int i = 0; i < newMatrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < newMatrix.GetLength(1); j++)
+        {
+            newMatrix[i, j] = matrix[j,i];
+        }
+    }
+
+Console.WriteLine();
+PrintMatrix(newMatrix);
