3d68144f94ee8c60ccdca2d58a70916b91270af3
naumovanatalia <naumova.na43@mail.ru>
2022-12-28 08:50:10 +0200

    add task57

---
+++ task57/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 57: Составить частотный словарь элементов двумерного массива. 
+// Частотный словарь содержит информацию о том, сколько раз встречается элемент входных данных.
+
+// метод для создания двумерного массива
+int[,] GetRandomMatrix(int rows, int columns, int leftRange = 0, int rightRange = 9)
+{
+    int[,] matr = new int[rows, columns];
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(leftRange, rightRange + 1);
+        }
+    }
+    return matr;
+}
+
+
+// метод для печати (вывода в консоль) двумерного массива
+void PrintMatrix(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        Console.WriteLine();
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write(matr[i, j] + " ");
+        }
+
+    }
+}
+
+
+int[,] matrix = GetRandomMatrix(3, 4);
+PrintMatrix(matrix);
+Console.WriteLine();
+int count = 0;
+for (int help = 0; help < 10; help++)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (matrix[i, j] == help)
+            {
+                count += 1;
+            }
+        }
+    }
+    Console.WriteLine($"В массиве {count} раз встречается {help}");
+    count = 0;
+}
